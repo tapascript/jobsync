49eee6fa2ddfca4b1a98ff6a104875e20fd15a06
Tapas Adhikary <tapas.adhikary@gmail.com>
2024-07-24 17:21:48 +0530

    (feat) Added Typescript to the project Added husky to the project Added Eslint Added prettier

---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: 'JobSync',
+  description: 'An initiative by the tapaScript Community',
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+  ],
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png',
+  },
+};
